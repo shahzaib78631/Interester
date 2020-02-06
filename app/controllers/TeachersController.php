93ce5a4ec09e037a7cd23293f3dad2fd9f40a429
shahzaib78631 <shahzaib78631@gmail.com>
2020-02-06 11:32:04 +0500

    updated

--- app/controllers/TeachersController.php
+++ app/controllers/TeachersController.php
@@ -7,7 +7,6 @@ class TeachersController extends Controller
         $this->head['title'] = 'Teachers';
         if($_SESSION['logged'] == true):
             $teachersManager = new TeachersModel;
-
             $this->view = 'teachers';
         else:
             $this->redirect('login');
